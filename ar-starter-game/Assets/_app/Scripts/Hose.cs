bc7b44bd00dbe42b7b8beb926051ad81efb6b937
Steve Leigh <steve.leigh@readify.net>
2020-01-31 15:58:11 +1000

    Quite possibly the worst hose ever

---
+++ ar-starter-game/Assets/_app/Scripts/Hose.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Hose : MonoBehaviour
+{
+    [SerializeField] float emissionRate = 1f;
+    
+    [SerializeField] GameObject waterDrop;
+    [SerializeField] float maxLifetime = 1;
+    [SerializeField] float emissionSpeed = 5;
+
+    void Update()
+    {
+        if (waterDrop == null)
+            return;
+        
+        if (Random.value < emissionRate * Time.deltaTime)
+        {
+            StartCoroutine(SpawnADrop());
+        }
+    }
+
+    private IEnumerator SpawnADrop()
+    {
+        var obj = Instantiate(waterDrop, transform.position, Quaternion.identity);
+        var rigidBody = obj.GetComponent<Rigidbody>();
+        rigidBody.velocity = (transform.up + Random.onUnitSphere*0.1f) * emissionSpeed;
+        yield return new WaitForSeconds(maxLifetime);
+        
+        Destroy(obj);
+    }
+}
