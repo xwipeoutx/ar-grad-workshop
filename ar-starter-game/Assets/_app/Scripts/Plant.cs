04146a75d9164c1d9b853235bfac29cb1488409a
Steve Leigh <steve.leigh@readify.net>
2020-02-07 13:10:05 +1000

    The hose grows.

---
+++ ar-starter-game/Assets/_app/Scripts/Plant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] LayerMask growthSource;
+    [SerializeField] float growthRate = 0.01f;
+    [SerializeField] float maxScale = 2f;
+
+    private Vector3 initialScale;
+    private float scaleMultiplier;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+        scaleMultiplier = 1;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if ((other.gameObject.layer & growthSource) == growthSource)
+            return;
+
+        scaleMultiplier += growthRate;
+
+        if (scaleMultiplier > maxScale)
+        {
+            enabled = false;
+            scaleMultiplier = maxScale;
+        }
+
+        transform.localScale = initialScale * scaleMultiplier;
+    }
+}
