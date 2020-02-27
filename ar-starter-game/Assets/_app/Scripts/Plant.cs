1e29dbf23507f362dfc36d7ba73f39119b448215
Steve Leigh <steve.leigh@readify.net>
2020-02-27 20:57:13 +1000

    Multiplayer plant creation

--- ar-starter-game/Assets/_app/Scripts/Plant.cs
+++ ar-starter-game/Assets/_app/Scripts/Plant.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Photon.Pun;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
-public class Plant : MonoBehaviour
+public class Plant : MonoBehaviour, IPunInstantiateMagicCallback
 {
     [SerializeField] LayerMask growthSource;
     [SerializeField] float growthRate = 0.01f;
@@ -30,5 +31,26 @@ public class Plant : MonoBehaviour
         }
 
         transform.localScale = initialScale * scaleMultiplier;
+    }
+
+    public void BeginLookingAwesome()
+    {
+        transform.Rotate(Vector3.up, Random.Range(0, 360));
+        transform.localScale = Vector3.one * Random.Range(0.8f, 1.1f);
+    }
+    
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        var instantiationData = info.photonView.InstantiationData;
+        var localPose = new Pose
+        {
+            position = (Vector3) instantiationData[0],
+            rotation = (Quaternion) instantiationData[1]
+        };
+
+        var sceneRoot = FindObjectOfType<SceneRoot>();
+        var worldPose = sceneRoot.transform.TransformPose(localPose);
+        transform.position = worldPose.position;
+        transform.rotation = worldPose.rotation;
     }
 }
--- ar-starter-game/Assets/_app/Scripts/PlantSpawner.cs
+++ ar-starter-game/Assets/_app/Scripts/PlantSpawner.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using Random = UnityEngine.Random;
 
 public class PlantSpawner : MonoBehaviour
 {
@@ -12,7 +15,7 @@ public class PlantSpawner : MonoBehaviour
     public void PlacePlant()
     {
         var ray = new Ray(camera.transform.position, camera.transform.forward);
-        
+
 #if UNITY_EDITOR
         if (!UnityEditor.EditorApplication.isRemoteConnected)
         {
@@ -21,7 +24,7 @@ public class PlantSpawner : MonoBehaviour
             return;
         }
 #endif
-        
+
         var raycastHits = new List<ARRaycastHit>();
         if (raycastManager.Raycast(ray, raycastHits))
         {
@@ -29,30 +32,49 @@ public class PlantSpawner : MonoBehaviour
             PlacePlant(pose);
         }
     }
-    
+
     void PlacePlant(Pose pose)
     {
         var plant = CreatePlant(pose);
-        plant.transform.Rotate(Vector3.up, Random.Range(0, 360));
-        plant.transform.localScale = Vector3.one * Random.Range(0.8f, 1.1f);
     }
 
     GameObject CreatePlant(Pose pose)
     {
+        var localPose = transform.InverseTransformPose(pose);
+
+        var instantiateParams = new object[]
+        {
+            localPose.position, 
+            localPose.rotation
+        };
+        
+        var plantGo = PhotonNetwork.InstantiateSceneObject("Plant", Vector3.zero, Quaternion.identity, data: instantiateParams);
+        var plant = plantGo.GetComponent<Plant>();
+        plant.BeginLookingAwesome();
+        return plantGo;
+
 #if UNITY_EDITOR
         if (!UnityEditor.EditorApplication.isRemoteConnected)
         {
             var parent = new GameObject();
             parent.transform.position = pose.position;
             parent.transform.rotation = pose.rotation;
-            
+
             var editorPlantObject = Instantiate(plantPrefab, parent.transform);
             return editorPlantObject;
         }
 #endif
-        
+
         var anchor = anchorManager.AddAnchor(pose);
         var plantObject = Instantiate(plantPrefab, anchor.transform);
         return plantObject;
-    } 
+    }
+}
+
+[Serializable]
+public struct PlantInstantiationData
+{
+    public Pose pose;
+    public float initialRotation;
+    public float initialScale;
 }
